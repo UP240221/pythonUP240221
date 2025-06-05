9b34546f1b19adb99c2006ab9ecbfabcbf46ea57
UP240221 <up240221@alumnos.upa.edu.mx>
2025-06-05 10:37:53 -0600

    creacion de archivo1

---
+++ Exerciseslevel1.py
@@ -0,0 +1,23 @@
+#Exercise level 1
+print(5+5) #addition(+)
+print(8-2) #subtraction(-)
+print(9*9) #multiplication(*)
+print(80%10) #modulus(%)
+print(80/2)  #division(/)
+print(9**8)  #exponential(**)
+print(40//2) #floor division operator(//)
+print(input('Your family name'))
+print[('Martin','Victoria','Brayan','Liliana','Elizabeth')]
+print(input('Enter your country'))
+print('Aguascalientes')
+print('I am enjoying 30 days of python')
+print(type(10))
+print(type (9.8))
+print(type (3.14))
+print(type (4-4j))
+print(type('Asabeneth','Python','finland'))
+print(type ('Martin'))
+print(type ('Victoria'.'Brayan','LIliana'))
+print(type ('Aguascalientes'))
+
+#Level 2#
