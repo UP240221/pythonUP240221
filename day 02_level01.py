68301496f1b99cdc9ba4e4d1892f276bee1bd7c3
UP240221 <up240221@alumnos.upa.edu.mx>
2025-06-10 17:10:41 -0600

    archivo day02 level 2 phython

---
+++ day 02_level01.py
@@ -0,0 +1,12 @@
+#Day 2: "30 Days of python programming"
+first_name = 'Martin'
+last_name = 'Emanuel'
+full_name = 'Martin Emanuel Perez Almaguer'
+country = 'AGS'
+city = 'Ags'
+age = '18 years'
+year = '2025'
+is_married = 'false'
+is_true = 'true'
+is_ligh_on = 'true'
+first_name, last_name, country, city = 'Martin', 'Emanuel' , 'AGS', 'Ags'
